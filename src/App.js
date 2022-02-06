2b4cb155daca4c9c98b2425200478221656a063d
kathir2602 <kathir@crayond.co>
2022-02-06 20:07:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import data from './data';
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Auto text Generator</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <button className="btn">Generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,13 @@
+const text = [
+  `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia provident enim doloribus facilis dolor? Quod ipsum suscipit eveniet eum aliquid consequuntur iure quis, eius sequi tempora illum velit accusantium perferendis.`,
+  `Unde voluptates tenetur inventore rerum? Perferendis officia at adipisci rerum optio facilis debitis minus reprehenderit, quae porro doloremque, similique est mollitia suscipit odit consectetur animi ratione exercitationem labore fugiat culpa.`,
+  `Dolorum, necessitatibus maxime. Non corrupti fuga error, fugit doloremque vel, doloribus labore rem ratione ducimus similique, magni eveniet voluptatum ad deleniti consequatur. Est dolorem itaque cum obcaecati tenetur temporibus quae?`,
+  `Minima facere ea eum explicabo, at doloribus reiciendis. Fugiat eius facere repudiandae ducimus doloribus asperiores soluta error voluptatem doloremque itaque, blanditiis, distinctio sed praesentium non recusandae modi, mollitia atque. Unde.`,
+  `Officia nesciunt voluptates, voluptatibus, odio nostrum voluptate iste itaque harum suscipit error, doloremque quae amet numquam quos? A, dolores earum. Odit laborum nesciunt id consectetur, dolorem totam culpa consequuntur aspernatur?`,
+  `Corporis, non dicta modi accusantium qui molestias officiis dolorum odio aliquam nesciunt animi, quasi eos doloribus quis unde deleniti sequi aspernatur consequatur pariatur suscipit. Accusantium, iusto vel. Ipsam, perferendis pariatur.`,
+  ` Eum dicta corrupti beatae voluptatibus, placeat repudiandae doloribus, molestiae repellat distinctio nam ipsam maiores aliquam sed rem? At placeat dignissimos autem. Modi est quia eius vel eum accusantium laudantium non!`,
+  `Facilis, cum maxime, et pariatur similique odio voluptate temporibus alias inventore maiores optio illo laboriosam eligendi id nemo quibusdam obcaecati repellat blanditiis! Dignissimos nihil a assumenda quidem qui dolor harum!`,
+  `Ea asperiores, ipsa, esse dolores aliquam quas quos corrupti officia sunt amet quidem provident explicabo? Excepturi fugit iusto delectus corrupti debitis quam, molestias, voluptates reiciendis maiores provident ducimus veniam id!`,
+];
+
+export default text;
